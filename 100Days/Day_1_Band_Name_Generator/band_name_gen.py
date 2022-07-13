7f2d49b3c6ce195a30d656aa74e6d9eb2e176baa
Kotmin <70173732+Kotmin@users.noreply.github.com>
2022-07-13 18:32:03 +0200

    Formatting fixing

--- 100Days/Day_1_Band_Name_Generator/band_name_gen.py
+++ 100Days/Day_1_Band_Name_Generator/band_name_gen.py
@@ -2,7 +2,7 @@ def return_band_name() -> str:
     return f'{input("Tell me name of city where you were born ")} {input("What was the name of your first pet? ")}'
 
 
-def show_band_name()-> None:
+def show_band_name() -> None:
     print(return_band_name())
     
 
