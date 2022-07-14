deaf7180924a2ebbb45841ddd6e03d75abd30637
Kotmin <70173732+Kotmin@users.noreply.github.com>
2022-07-14 10:30:18 +0200

    with_map

--- 100Days/Day_2_Sum_of_Digits/sum_of_digits.py
+++ 100Days/Day_2_Sum_of_Digits/sum_of_digits.py
@@ -1,11 +1,11 @@
 # 🚨 Don't change the code below 👇
-two_digit_number = input("Type a two digit number: ")
+# two_digit_number = input("Type a two digit number: ")
 # 🚨 Don't change the code above 👆
 
 ####################################
 #Write your code below this line 👇
 
-def sum_of_pieces(x:str) -> Integer:
+def sum_of_pieces(x):
     y=int(x)
     sum=0
     while(y>0):
@@ -13,5 +13,6 @@ def sum_of_pieces(x:str) -> Integer:
         y=(y//10)
     return sum
 
-
-print(sum_of_pieces(two_digit_number))
+lista = ("10","52","92")
+#print(sum_of_pieces(two_digit_number))
+print(list(map(sum_of_pieces,lista)))
