e6396fa80fc77fbc8f0af12ce302088729a00dca
Kotmin <70173732+Kotmin@users.noreply.github.com>
2022-07-15 20:40:42 +0200

    brothers_quest_game_init

---
+++ 100Days/Day_3_brothers_game/brothers_quest.py
@@ -0,0 +1,41 @@
+print('''
+
+
+
+                       ,sdPBbs.				                            	 (      /|\      .    |      \      .   +
+                      ,d$$$$$$$$b.				                                 . |||||     _    | |   | | ||         .
+                     d$P'`Y'`Y'`?$b				                            .      |||||    | |  _| | | | |_||    .
+                    d'    `  '  \ `b				                        .      |||||    | |  _| | | | |_||    .
+                   /    |        \  \			                        	__||||_|||||____| |_|_____________\__________
+                  /    / \       |   \			                        	. |||| |||||  /\   _____      _____  .   .
+             _,--'        |      \    |				                          |||| ||||| ||||   .   .  .         ________
+           /' _/          \   |        \			                         . \|`-'|||| ||||    __________       .    .
+        _/' /'             |   \        `-.__			                        \__ |||| ||||      .          .     .
+    __/'       ,-'    /    |    |     \      `--...__		                  __    ||||`-'|||  .       .    __________
+  /'          /      |    / \     \     `-.           `\	                 .    . |||| ___/  ___________             .
+ /    /;;,,__-'      /   /    \            \            `-.	                    . _ ||||| . _               .   _________
+/    |;;;;;;;\                                             \ 	             _   ___|||||__  _           .          _	
+
+	
+
+''')
+
+print('''
+      You stand at a crossroads. You see two paths ahead of you through forrests and dessert. You know that only one of them will get you safely to your destination, the other will be your undoing.
+
+Two brothers are standing nearby. One of them is always telling the truth, the other is always lying. You need to ask both of them the same question on the basis of which you will find out which way to go. Remember they are mean, you only get one chance.
+
+What question will you ask?
+      
+      ''')
+
+ans=input(" > ")
+
+# Which path would your brother choose?
+
+# Which path would your brother choose?
+if(ans.strip().lower().replace(' ','')=="whichpatchwouldyourbrotherchoose?"):
+  print("Great question, you're great pretender")
+else:
+  print("Well it didn't went well")
+  
