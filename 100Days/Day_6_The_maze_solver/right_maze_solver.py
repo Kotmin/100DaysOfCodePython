da685fc88e4f98ea5c585700d72cc15fb06844d2
Kotmin <70173732+Kotmin@users.noreply.github.com>
2022-07-19 14:45:43 +0200

    Resolved_maze_?

--- 100Days/Day_6_The_maze_solver/right_maze_solver.py
+++ 100Days/Day_6_The_maze_solver/right_maze_solver.py
@@ -1,1 +1,18 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+while not at_goal():
+    if wall_on_right() and front_is_clear():
+        move()
+    if wall_on_right() and  not front_is_clear():
+        turn_left()
+    if not wall_on_right() and not at_goal():
+        turn_right()
+        move()
+
+
+# I'm afraid there should be only one case when we're creating infinite loop. Needs to be tested
