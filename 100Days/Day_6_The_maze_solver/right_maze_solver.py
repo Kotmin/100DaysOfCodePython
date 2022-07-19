5c47d3c42baaa45b38fba44a880e263b82b8db32
Kotmin <70173732+Kotmin@users.noreply.github.com>
2022-07-19 15:13:18 +0200

    Fix fist amendment

--- 100Days/Day_6_The_maze_solver/right_maze_solver.py
+++ 100Days/Day_6_The_maze_solver/right_maze_solver.py
@@ -16,3 +16,34 @@ while not at_goal():
 
 
 # I'm afraid there should be only one case when we're creating infinite loop. Needs to be tested
+
+
+
+## Updated version for resolveing infinite loop problem
+
+# First idea add left_is_clear_state
+
+# def turn_right():
+#     for i in range(3):
+#         turn_left()
+
+# def left_is_clear():
+#     turn_left()
+#     state = front_is_clear()
+#     turn_right()
+#     return state
+        
+# while not at_goal():
+#     if wall_on_right() and front_is_clear():
+#         move()
+#     if wall_on_right() and  not front_is_clear():
+#         turn_left()
+#     if left_is_clear() and right_is_clear() and not at_goal():
+#         turn_left()
+#         move()
+#     if not wall_on_right() and not at_goal():
+#         turn_right()
+#         move()
+
+
+##
